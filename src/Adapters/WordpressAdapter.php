56a25f9302fc4596da602a5be0ce67dff1b79373
Jordy Manner <jordy.manner@milkcreation.fr>
2021-01-04 19:05:39 +0000

    contact-infos 1.0.x-dev

--- src/Adapters/WordpressAdapter.php
+++ src/Adapters/WordpressAdapter.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Pollen\ContactInfos\Adapters;
 
 use Pollen\ContactInfos\Contracts\ContactInfosContract;
-use tiFy\Support\Proxy\Metabox;
 
 class WordpressAdapter extends AbstractContactInfosAdapter
 {
@@ -16,35 +15,48 @@ class WordpressAdapter extends AbstractContactInfosAdapter
     {
         parent::__construct($cinfosManager);
 
-        add_action('init', function () {
-            if ($config = config('contact-infos', true)) {
-                $defaults = [
-                    'admin' => false,
-                ];
+        add_action(
+            'init',
+            function () {
+                if ($config = config('contact-infos', true)) {
+                    $defaults = [
+                        'admin' => false,
+                    ];
 
-                config([
-                           'contact-infos' => is_array($config) ? array_merge($defaults, $config) : $defaults,
-                       ]);
+                    config(
+                        [
+                            'contact-infos' => is_array($config) ? array_merge($defaults, $config) : $defaults,
+                        ]
+                    );
 
-                if ($admin = config('contact-infos.admin')) {
-                    $defaults = ['params' => []];
+                    if ($admin = config('contact-infos.admin')) {
+                        $defaults = ['params' => []];
 
-                    if ($fields = config('contact-infos.fields', [])) {
-                        $defaults['params']['fields'] = $fields;
+                        if ($fields = config('contact-infos.fields', [])) {
+                            $defaults['params']['fields'] = $fields;
+                        }
+
+                        if ($groups = config('contact-infos.groups', [])) {
+                            $defaults['params']['groups'] = $groups;
+                        }
+
+                        $attrs = is_array($admin) ? array_merge($defaults, $admin) : $defaults;
+                        $attrs['driver'] = 'contact-infos';
+
+                        $this->cinfos()->metaboxManager()->add(
+                            md5('ContactInfosMetabox'),
+                            array_merge(
+                                $attrs,
+                                [
+                                    'driver' => 'contact-infos',
+                                ]
+                            ),
+                            'tify_options@options',
+                            'tab'
+                        );
                     }
-
-                    if ($groups = config('contact-infos.groups', [])) {
-                        $defaults['params']['groups'] = $groups;
-                    }
-
-                    $attrs = is_array($admin) ? array_merge($defaults, $admin) : $defaults;
-                    $attrs['driver'] = 'contact-infos';
-
-                    Metabox::add('ContactInfos', array_merge($attrs, [
-                        'driver' => 'contact-infos',
-                    ]))->setScreen('tify_options@options')->setContext('tab');
                 }
             }
-        });
+        );
     }
 }
